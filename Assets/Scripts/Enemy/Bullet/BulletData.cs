94449450da191d7269d362fb2eef3c0a011a188d
switchiz <starter128@gmail.com>
2024-04-03 14:22:48 +0900

    황호연 _ x _ 기타수정

--- Assets/Scripts/Enemy/Bullet/BulletData.cs
+++ Assets/Scripts/Enemy/Bullet/BulletData.cs
@@ -11,7 +11,8 @@ public class BulletData : ScriptableObject
     public BulletType bulletType;       // Bullet 타입
     public float moveSpeed = 5;         // Bullet 속도
     public uint bulletDamage = 1;       // Bullet 데미지
-    public float bulletSize = 0.3f;
+    public float bulletSize = 0.3f;     // Bullet 크기
+    public float lifeTime = 10.0f;      // Bullet 생존 시간
     public bool isThrougt;              // Bullet 의 관통 여부
     public bool isParring;              // Bullet 의 패링 여부
     public bool isPlayer;               // Bullet 의 적/아군 여부
