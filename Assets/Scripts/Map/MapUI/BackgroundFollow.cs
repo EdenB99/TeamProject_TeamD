e4e90b80c26f35dc6df19b3335ff7377c1a5be8f
bamdal <alstn7695@gmail.com>
2024-05-14 15:14:40 +0900

    손진웅 - 맵 스크립트 수정
    특정 맵마다 특정 배경이미지 지정

--- Assets/Scripts/Map/MapUI/BackgroundFollow.cs
+++ Assets/Scripts/Map/MapUI/BackgroundFollow.cs
@@ -1,26 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BackgroundFollow : MonoBehaviour
 {
     public Transform background;
-    public SpriteRenderer backgroundSprite;
+    public Sprite[] backgrounds;
     Camera mainCam;
+
+    Dictionary<string, int> sceneToBackgroundIndex;
 
     private void Start()
     {
         mainCam = Camera.main;
-        backgroundSprite = GetComponent<SpriteRenderer>();
+        sceneToBackgroundIndex = new Dictionary<string, int>()
+        {
+            {"FirstLoadScene", 0},  // Å×½ºÆ®
+            {"Tutorial", 1},
+        };
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
+        if (sceneToBackgroundIndex.TryGetValue(currentSceneName, out int backgroundIndex))
+        {
+            GetComponent<SpriteRenderer>().sprite = backgrounds[backgroundIndex];
+        }
     }
 
     private void LateUpdate()
     {
         background.transform.position = new Vector2(mainCam.transform.position.x *0.8f, mainCam.transform.position.y*0.9f);
-    }
-
-    void backgroundRefresh(Sprite sprite)
-    {
-        backgroundSprite.sprite = sprite;
     }
 }
