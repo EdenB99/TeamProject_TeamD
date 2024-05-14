570e70f22d34b57247b309237c8a1a0543b0fc51
bamdal <alstn7695@gmail.com>
2024-05-14 14:50:40 +0900

    손진웅 - 맵 배경 수정

--- Assets/Scripts/Map/MapUI/BackgroundFollow.cs
+++ Assets/Scripts/Map/MapUI/BackgroundFollow.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class BackgroundFollow : MonoBehaviour
 {
     public Transform background;
+    public SpriteRenderer backgroundSprite;
     Camera mainCam;
 
     private void Start()
     {
         mainCam = Camera.main;
+        backgroundSprite = GetComponent<SpriteRenderer>();
     }
 
     private void LateUpdate()
     {
-        background.transform.position = new Vector2(mainCam.transform.position.x *0.8f, mainCam.transform.position.y);
+        background.transform.position = new Vector2(mainCam.transform.position.x *0.8f, mainCam.transform.position.y*0.9f);
+    }
+
+    void backgroundRefresh(Sprite sprite)
+    {
+        backgroundSprite.sprite = sprite;
     }
 }
