b372550717e55f2f27675e9c3ffde106eaed5167
jinwoong0217 <153969837+jinwoong0217@users.noreply.github.com>
2024-05-14 18:32:23 +0900

    손진웅 - 맵 배경
    스크립트 수정 및 배경 이미지 추가

--- Assets/Scripts/Map/MapUI/BackgroundFollow.cs
+++ Assets/Scripts/Map/MapUI/BackgroundFollow.cs
@@ -7,28 +7,39 @@ public class BackgroundFollow : MonoBehaviour
 {
     public Transform background;
     public Sprite[] backgrounds;
-    Camera mainCam;
+    private Camera mainCam;
 
-    Dictionary<string, int> sceneToBackgroundIndex;
-
-    private void Start()
+    private void Awake()
     {
         mainCam = Camera.main;
-        sceneToBackgroundIndex = new Dictionary<string, int>()
-        {
-            {"FirstLoadScene", 0},  // 테스트
-            {"Tutorial", 1},
-        };
+        SceneManager.sceneLoaded += OnSceneLoaded; // 씬이 로딩될 때마다 함수 호출
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        UpdateBackground();
+    }
+
+    void UpdateBackground()
+    {
         string currentSceneName = SceneManager.GetActiveScene().name;
 
-        if (sceneToBackgroundIndex.TryGetValue(currentSceneName, out int backgroundIndex))
+        if (currentSceneName == "Tutorial")
         {
-            GetComponent<SpriteRenderer>().sprite = backgrounds[backgroundIndex];
+            GetComponent<SpriteRenderer>().sprite = backgrounds[1];
+        }
+        else if (currentSceneName == "Town")
+        {
+            GetComponent<SpriteRenderer>().sprite = backgrounds[2];
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = backgrounds[0];
         }
     }
 
     private void LateUpdate()
     {
-        background.transform.position = new Vector2(mainCam.transform.position.x *0.8f, mainCam.transform.position.y*0.9f);
+        background.transform.position = new Vector2(mainCam.transform.position.x * 0.8f, mainCam.transform.position.y * 0.9f);
     }
 }
