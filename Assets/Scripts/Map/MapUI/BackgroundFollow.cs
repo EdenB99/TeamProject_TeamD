dafadab929b1a443a511de688d507f6e6fd34e14
Jinwoong <jinwoong0326@gmail.com>
2024-05-17 12:59:39 +0900

    손진웅 - 배경 스크립트 수정

--- Assets/Scripts/Map/MapUI/BackgroundFollow.cs
+++ Assets/Scripts/Map/MapUI/BackgroundFollow.cs
@@ -7,11 +7,13 @@ public class BackgroundFollow : MonoBehaviour
 {
     public Transform background;
     public Sprite[] backgrounds;
-    private Camera mainCam;
+    public GameObject boundaryObject;
+    BoxCollider2D boundaryCollider;
+    Camera maincamera;
 
     private void Awake()
     {
-        mainCam = Camera.main;
+        maincamera = Camera.main;
         SceneManager.sceneLoaded += OnSceneLoaded; // 씬이 로딩될 때마다 함수 호출
     }
 
@@ -24,13 +26,9 @@ public class BackgroundFollow : MonoBehaviour
     {
         string currentSceneName = SceneManager.GetActiveScene().name;
 
-        if (currentSceneName == "Tutorial")
+        if (currentSceneName == "Town" || currentSceneName == "Tutorial")
         {
             GetComponent<SpriteRenderer>().sprite = backgrounds[1];
-        }
-        else if (currentSceneName == "Town")
-        {
-            GetComponent<SpriteRenderer>().sprite = backgrounds[2];
         }
         else
         {
@@ -40,6 +38,43 @@ public class BackgroundFollow : MonoBehaviour
 
     private void LateUpdate()
     {
-        background.transform.position = new Vector2(mainCam.transform.position.x * 0.8f, mainCam.transform.position.y * 0.9f + 1.5f);
+        if (boundaryCollider == null)
+        {
+            FindBoundaryObject();
+        }
+        UpdateBackgroundPosition();
+    }
+
+    private void FindBoundaryObject()
+    {
+        GameObject foundBoundaryObject = GameObject.FindGameObjectWithTag("Boundary");
+        if (foundBoundaryObject != null)
+        {
+            boundaryObject = foundBoundaryObject;
+            boundaryCollider = boundaryObject.GetComponent<BoxCollider2D>();
+        }
+    }
+
+    public float lerpSpeed = 2.0f;
+
+    float backgroundWidth = 3;
+    float backgroundHeight = 5;
+
+    void UpdateBackgroundPosition()
+    {
+        Vector2 backgroundPosition = background.position;
+
+        Bounds bounds = boundaryCollider.bounds;
+
+        backgroundPosition.x = Mathf.Clamp(backgroundPosition.x, bounds.min.x + backgroundWidth / 2, bounds.max.x - backgroundWidth / 2);
+        backgroundPosition.y = Mathf.Clamp(backgroundPosition.y, bounds.min.y + backgroundHeight / 2, bounds.max.y - backgroundHeight / 2);
+
+        background.position = backgroundPosition;
+        background.transform.position = Vector2.Lerp(background.transform.position, backgroundPosition, lerpSpeed * Time.deltaTime);
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
