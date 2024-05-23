df6b8563ff74335ae4620b455942ab82dba1a092
bamdal <alstn7695@gmail.com>
2024-05-23 15:04:28 +0900

    손진웅 - 무기 스킬 이펙트
    스킬 키를 누르면 호출하는 함수 생성

--- Assets/Scripts/Weapon/WeaponBase.cs
+++ Assets/Scripts/Weapon/WeaponBase.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
@@ -23,7 +24,7 @@ public class WeaponBase : MonoBehaviour
 {
     new Rigidbody2D rigidbody;
     protected Animator animator;
-    WeaponAction weaponInputActions;
+    protected WeaponAction weaponInputActions;
 
     SpriteRenderer spriteRenderer;
 
@@ -47,6 +48,7 @@ public class WeaponBase : MonoBehaviour
     /// </summary>
     [SerializeField]
     private GameObject weaponEffectPrefab;
+    GameObject weaponSkillPrefab;
 
     /// <summary>
     /// 이펙트 생성좌표
