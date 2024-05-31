73ea920dcb6177032a8018912c30ea4bf07d4f91
han <hunlung123@naver.com>
2024-05-31 13:19:35 +0900

    함정총알이 벽을 뚫는 오류, 모든맵에 빠른이동 포탈이 있는 오류 수정

--- Assets/Scripts/Map/MapManager/MapManager.cs
+++ Assets/Scripts/Map/MapManager/MapManager.cs
@@ -15,15 +15,15 @@ using Random = UnityEngine.Random;
 
 public class MapManager : MonoBehaviour
 {
-
     //TODO:: 플레이어 사망시, 게임 클리어 시 결산 필요(플레이어체력,상태 초기화,인벤토리 초기화)
-    //TODO:: 현재 맵UI에 인터랙티브포탈을 통해 빠른이동이 되는 중,아이콘이 여러개 일 시 겹쳐지는 오류 존재
-    //       클릭을 위한 무언가의 이미지가 필요함 (MapUI에)
+
+    //TODO:: 아이콘이 여러개 일 시 겹쳐지는 오류 존재, 클릭을 위한 무언가의 이미지가 필요함 (MapUI에)
     //TODO:: 맵에 맵25에서 위포탈로 맵이만들어졌지만 해당맵에 밑포탈이 없어 연결끊김, 해당 맵에서 왼쪽으로 
     //       맵이 두개 만들어졌지만 두개 다 밑의 맵에 위 포탈이 없어 이어지지 않아, 3개의 빈 맵이 만들어지게 됨.
     //       하지만3개는 이어져있기 때문에 삭제되지도않고 그대로 맵생성 및 다음맵을 받아 다음스테이지로 넘어가지 못함
 
     //보상 상자가 맵나갔다오면 사라짐(chest로 받아둬서 null 발생)
+    // 25map의 chest가 초기화 되는 오류
     /*
 
     발생한 오류---
@@ -43,8 +43,6 @@ WeaponBase.UpdateWeaponPosition () (at Assets/Scripts/Weapon/WeaponBase.cs:199)
 WeaponBase.Update () (at Assets/Scripts/Weapon/WeaponBase.cs:182)
 SlashWeapon.Update () (at Assets/Scripts/Weapon/WeaponType/SlashWeapon.cs:62)
             transform.position = hinge.position;
-
-    10.25map의 chest가 초기화
     
 
 */
@@ -58,6 +56,7 @@ SlashWeapon.Update () (at Assets/Scripts/Weapon/WeaponType/SlashWeapon.cs:62)
     5. 튜토리얼의 rightPortal이 작동하지 않는 오류
     6. npc를 만난 후 f를 누르면 전에 대화한 npc와의 대화창이 작동함,상자도 마찬가지
     7. 빠른맵 이동이 적이 있어도 사용가능한 것, 아이템 저장 및 총알 삭제
+    8. 빠른이동시 아이템에 문제가 생기는 오류
 
     */
 
@@ -418,7 +417,7 @@ SlashWeapon.Update () (at Assets/Scripts/Weapon/WeaponType/SlashWeapon.cs:62)
             }
 
             //4번째 맵마다 빠른이동용 포탈 활성화
-            if (mapIndex % 1 == 0 || mapIndex == 1 || mapIndex == 6)
+            if (mapIndex % 4 == 0 || mapIndex == 1 || mapIndex == 6)
             {
                 mapData.hasQuickPortal = true;
                 Debug.Log($"뒤에서 {mapIndex}번째 맵,  {mapData.sceneName}씬에 포탈생성."); // 맵 인덱스 출력
